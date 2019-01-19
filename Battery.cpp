f13c1fd449b033d4aa9874dc275b3bc00ef21af5
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-01-19 18:25:30 +0000

    Update Battery.cpp

--- Battery.cpp
+++ Battery.cpp
@@ -8,7 +8,7 @@ long Battery::read() {
   ADCSRA |= _BV(ADSC);
   while (bit_is_set(ADCSRA, ADSC));
   _val = 1126400L / (ADCL | (ADCH << 8));
-  _percent = (_val * 100) / (float)max(min(_val, high), high);
+  _percent = (_val * 100) / (float)max(_val, high);
   _level = map(_val, low, high, 0, full);
   return _val;
 }

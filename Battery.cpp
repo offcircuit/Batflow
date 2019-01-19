189d46b208d9eedd17d1a093dea01d52db83a0b6
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-01-19 18:35:50 +0000

    Update Battery.cpp

--- Battery.cpp
+++ Battery.cpp
@@ -2,7 +2,7 @@
 
 // PUBLIC
 
-long Battery::read() {
+Battery Battery::read() {
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
   delay(2);
   ADCSRA |= _BV(ADSC);
@@ -10,7 +10,7 @@ long Battery::read() {
   _val = 1126400L / (ADCL | (ADCH << 8));
   _percent = (_val * 100) / (float)max(_val, high);
   _level = map(_val, low, high, 0, full);
-  return _val;
+  return *this;
 }
 
 // PRIVATE

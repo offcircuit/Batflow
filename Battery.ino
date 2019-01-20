3f4a50e2c6ee4317c0b6f59c23a92a366ec4076c
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-01-20 14:44:06 +0000

    Update Battery.ino

--- Battery.ino
+++ Battery.ino
@@ -1,22 +1,19 @@
 #include "Battery.h"
 
-float l[5] = {3.2, 3.8, 4.4, 4.7, 5};
-Battery b(8046);
+Battery b(5160, 5200);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
-  Serial.println("----------avr-------------");
-  Serial.println("---------class--------------");
-  Serial.println("----------read-------------");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print(b.read());
-  Serial.print("----");
-  Serial.println(b.percent);
-  Serial.println(b.level);
-  delay(2000);
+  Serial.print(b.read().val);
+  Serial.print("mV ---- ");
+  Serial.print(b.percent);
+  Serial.print("% ---- ");
+  Serial.print(b.level);
+  Serial.println("_");
+  delay(100);
 }
